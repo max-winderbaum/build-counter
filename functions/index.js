8ae00c38bce59470f90612817f32c2dcc7295267
Max Winderbaum <maxwinderbaum@gmail.com>
2017-06-12 11:36:05 -0700

    Add functional test retries

--- functions/index.js
+++ functions/index.js
@@ -18,3 +18,19 @@ exports.onbuild = functions.https.onRequest((req, res) => {
     });
 });
 
+exports.onfunctionaltestretry = functions.https.onRequest((req, res) => {
+    const builds = admin.database().ref('functional_test_retry_count');
+    const buildLog = admin.database().ref('functional_test_retries');
+
+    const buildLogRef = buildLog.push();
+    buildLogRef.set({
+        'date': new Date().toUTCString(),
+    });
+
+    builds.transaction(function(numBuilds) {
+        return (numBuilds || 0) + 1;
+    }, function(error, currentData) {
+        res.status(200).send(currentData);
+    });
+});
+
